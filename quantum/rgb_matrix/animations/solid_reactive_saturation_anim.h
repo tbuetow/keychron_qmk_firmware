927539ef50b2566f1c6c08699ce7888b8d665bff
Thomas Buetow <thomas.buetow@gmail.com>
2025-01-04 10:34:58 -0500

    Update solid_reactive_saturation_anim to greater desaturation, add reversed effect, and better description.

--- quantum/rgb_matrix/animations/solid_reactive_saturation_anim.h
+++ quantum/rgb_matrix/animations/solid_reactive_saturation_anim.h
@@ -1,4 +1,5 @@
-// Start at desaturated base color and saturate pressed key, fading back to desaturated.
+// Single key reactive saturation effects. Desaturates the key pressed before fading back to base color.
+// Can be reversed, saturating each key from a heavily desaturated base color.
 // How to set baseline desaturation?
 
 #ifdef RGB_MATRIX_KEYREACTIVE_ENABLED
@@ -10,8 +11,8 @@ static HSV SOLID_REACTIVE_SATURATION_math(HSV hsv, uint16_t offset) {
 #            ifdef RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
     hsv.h = scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 8) >> 4);
 #            endif
-    // hsv.h += qsub8(130, offset);
-    hsv.s -= qsub8(offset, 130);
+    // hsv.s -= qsub8(offset, 20); //white to saturated color
+    hsv.s += qadd8(20, offset); //saturated color to white
     return hsv;
 }
 
